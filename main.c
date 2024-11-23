8bce2449b4317bba2720d6ac8c6683d4c19e8044
DanWlker <danielhee2@gmail.com>
2024-11-23 14:20:08 +0800

    Updates

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
+#include <stdbool.h>  // wtf? this is needed?
+#include <stdint.h>
 #include <stdio.h>
 
 #include "src/exercise.h"
 #include "src/thing.h"
 
 int main() {
-  printf("%f\n", get_average(4, 3, 4));
-  printf("%s\n", shout());
+  printf("sizeof(char)   = %zu\n", sizeof(char));
+  printf("sizeof(int)   = %zu\n", sizeof(int));
+  printf("sizeof(float)   = %zu\n", sizeof(float));
+  printf("sizeof(double)   = %zu\n", sizeof(double));
+  printf("sizeof(size_t)   = %zu\n", sizeof(size_t));
+  printf("sizeof(bool)   = %zu\n", sizeof(bool));
 }
